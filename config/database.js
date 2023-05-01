3cc3db87316e4c682d6f0e1490ce4c4ca7ba56a2
AmaurySilverio <amaurycodes@gmail.com>
2023-05-01 00:02:02 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+  url: "mongodb+srv://amaurycodes:Cofwp0oLYdFZk01x@cluster0.9qj4jet.mongodb.net/dreamJournal?retryWrites=true&w=majority",
+  dbName: "dreamJournal",
+};
